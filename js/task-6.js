1f97569fd388c17e37340ca221ed8a1d76a740ab
AnShatylo <a.shatilo18@gmail.com>
2024-06-14 19:24:58 +0200

    V2

--- js/task-6.js
+++ js/task-6.js
@@ -1,39 +1,38 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
-const createBtn = document.querySelector("#controls button[data-create]");
-const destroyBtn = document.querySelector("#controls button[data-destroy]");
-const boxesWrapp = document.querySelector("#boxes");
-
-function createBoxes(amount) {
-  boxesWrapp.innerHTML = "";
-
-  let step = 30;
-  for (let i = 0; i < amount; i++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.style.backgroundColor = getRandomHexColor();
-    box.style.width = `${step}px`;
-    box.style.height = `${step}px`;
-    boxesWrapp.appendChild(box);
-    step += 10;
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
   }
 
-  document.querySelector("#controls input").value = "";
-}
+  const createBtn = document.querySelector("#controls button[data-create]");
+  const destroyBtn = document.querySelector("#controls button[data-destroy]");
+  const boxesWrapp = document.querySelector("#boxes");
 
-createBtn.addEventListener("click", () => {
-  const amoutOfBoxes = parseInt(
-    document.querySelector("#controls input").value
-  );
-  if (amoutOfBoxes >= 1 && amoutOfBoxes <= 100) {
-    createBoxes(amoutOfBoxes);
-  } 
-});
+  function createBoxes(amount) {
+    boxesWrapp.innerHTML = "";
 
-destroyBtn.addEventListener("click", () => {
-  boxesWrapp.innerHTML = "";
-});
+    let step = 30;
+    let markup = '';
+    for (let i = 0; i < amount; i++) {
+      markup += `
+      <div class="box" style="background-color: ${getRandomHexColor()}; width: ${step}px; height: ${step}px;"></div>
+    `;
+      step += 10;
+    }
+
+    boxesWrapp.insertAdjacentHTML('beforeend', markup);
+    document.querySelector("#controls input").value = "";
+  }
+
+  createBtn.addEventListener("click", () => {
+    const amoutOfBoxes = parseInt(
+      document.querySelector("#controls input").value
+    );
+    if (amoutOfBoxes >= 1 && amoutOfBoxes <= 100) {
+      createBoxes(amoutOfBoxes);
+    } 
+  });
+
+  destroyBtn.addEventListener("click", () => {
+    boxesWrapp.innerHTML = "";
+  });
